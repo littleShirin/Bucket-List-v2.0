bb3cbc5d16deec4577d673799b10247fd24c2bd3
Shirin Little <shirinlittle94@gmail.com>
2020-11-05 16:08:38 -0500

    testing

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config(); // instead of saving it 
+require('dotenv').config(); // instead of saving it
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -30,59 +30,60 @@ app.use('/list', listRouter);
 app.use('/auth', authRouter);
 
 passport.serializeUser((user, done) => {
-	done(null, user);
+  done(null, user);
 });
 
 passport.deserializeUser((id, done) => {
-	User.findById(id).then((user) => {
-		done(null, user);
-	});
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
 });
 
 app.use(passport.initialize());
 
 passport.use(
-	new GoogleStrategy(
-		{
-			clientID: process.env.GOOGLE_CLIENT_ID,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-			callbackURL: '/auth/google/callback',
-		},
-		//acessToken: app use to make API request on behalf od user
-		//refreshToken: used to allow an app to obtain a new access token without prompting the user
-		//done is called to tell passport that we have finished looking up or creating a user
-		function (accessToken, refreshToken, profile, done) {
-			//passport callback function
-			console.log('inside express');
-			// should have the part where we add users in db somewhere here
-			// console.log(profile);
-			// User.findOrCreate({ GoogleId: profile.id }, function (err, user) {
-			// 	console.log('this is where we get stuck');
-			// 	return done(err, user);
-			// });
-			// console.log('this is the outside');
-			User.find({ GoogleId: profile.id })
-				.then((currentUser) => {
-					if (currentUser) {
-						//if we already have a record with the given profile ID
-						done(null, currentUser);
-					} else {
-						//if not, create a new user
-						new User({
-							GoogleId: profile.id,
-							DisplayName: profile.displayName,
-							Image: profile.photos[0].value,
-						})
-							.save()
-							.then((newUser) => {
-								done(null, newUser);
-							})
-							.catch(() => console.log('error'));
-					}
-				})
-				.catch(() => console.log('error in finding user'));
-		}
-	)
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback',
+    },
+    //acessToken: app use to make API request on behalf od user
+    //refreshToken: used to allow an app to obtain a new access token without prompting the user
+    //done is called to tell passport that we have finished looking up or creating a user
+    function (accessToken, refreshToken, profile, done) {
+      //passport callback function
+      console.log('inside express');
+      // should have the part where we add users in db somewhere here
+      // console.log(profile);
+      // User.findOrCreate({ GoogleId: profile.id }, function (err, user) {
+      // 	console.log('this is where we get stuck');
+      // 	return done(err, user);
+      // });
+      // console.log('this is the outside');
+      User.find({ GoogleId: profile.id })
+        .then((currentUser) => {
+          if (currentUser) {
+            //if we already have a record with the given profile ID
+            done(null, currentUser);
+          } else {
+            //if not, create a new user
+            new User({
+              GoogleId: profile.id,
+              DisplayName: profile.displayName,
+              Image: profile.photos[0].value,
+            })
+              .save()
+              .then((newUser) => {
+                console.log('new user', newUser);
+                done(null, newUser);
+              })
+              .catch(() => console.log('error'));
+          }
+        })
+        .catch(() => console.log('error in finding user'));
+    }
+  )
 );
 
 // GET /auth/google
@@ -91,11 +92,11 @@ passport.use(
 //   the user to google.com.  After authorization, Google will redirect the user
 //   back to this application at /auth/google/callback
 app.get(
-	'/auth/google',
-	passport.authenticate('google', {
-		scope: ['https://www.googleapis.com/auth/plus.login'],
-		prompt: 'select_account',
-	})
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['https://www.googleapis.com/auth/plus.login'],
+    prompt: 'select_account',
+  })
 );
 
 // GET /auth/google/callback
@@ -104,17 +105,15 @@ app.get(
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
 app.get(
-	'/auth/google/callback',
-	passport.authenticate('google', { failureRedirect: '/login' }),
-	function (req, res) {
-		res.redirect('/');
-	}
+  '/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function (req, res) {
+    res.redirect('/');
+  }
 );
 
-
-
 app.get('/', (req, res) =>
-	res.status(200).sendFile(path.resolve(__dirname, '../index.html'))
+  res.status(200).sendFile(path.resolve(__dirname, '../index.html'))
 );
 
 // catch-all route handler for any requests to an unknown route
@@ -122,18 +121,18 @@ app.use((req, res) => res.sendStatus(404));
 
 // global middleware error handler
 app.use((err, req, res, next) => {
-	const defaultErr = {
-		log: 'Express error handler caught unknown middleware error',
-		status: 400,
-		message: { err: 'An error occurred' },
-	};
-	const errorObj = { ...defaultErr, ...err };
-	console.log(errorObj.log);
-	return res.status(errorObj.status).json(errorObj.message);
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = { ...defaultErr, ...err };
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
 });
 
 // start server
-app.listen(3000, (err) => {
-	if (err) return console.log(err);
-	console.log('Server running and listening on 3000');
+app.listen(3003, (err) => {
+  if (err) return console.log(err);
+  console.log('Server running and listening on 3000');
 });
--- webpack.config.js
+++ webpack.config.js
@@ -2,42 +2,43 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-	entry: ['./client/index.js'],
-	output: {
-		path: path.resolve(__dirname, './dist'),
-		filename: 'bundle.js',
-	},
-	resolve: {
-		extensions: ['.js', '.jsx'],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				use: ['babel-loader'],
-			},
-			{
-				test: /\.(scss|css)$/,
-				exclude: /node_modules/,
-				use: ['style-loader', 'css-loader'],
-			},
-		],
-	},
-	devServer: {
-		port: 8080,
-		open: true,
-		hot: true,
-		publicPath: '/dist/',
-		proxy: [
-			{ '/' : 'http://localhost:3000' // added in to not have to type out entire url. just end routes 
-		}, 
-			{
-				context: ['/auth/google'],
-				target: 'http://localhost:3000',
-			},
-		],
-		historyApiFallback: true
-	},
-	plugins: [new webpack.HotModuleReplacementPlugin()],
-}
+  entry: ['./client/index.js'],
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(scss|css)$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    port: 8080,
+    open: true,
+    hot: true,
+    publicPath: '/dist/',
+    proxy: [
+      {
+        '/': 'http://localhost:3003', // added in to not have to type out entire url. just end routes
+      },
+      {
+        context: ['/auth/google'],
+        target: 'http://localhost:3003',
+      },
+    ],
+    historyApiFallback: true,
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+};
